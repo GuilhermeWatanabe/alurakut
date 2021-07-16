6cc036e74f0970a231dbcdfb77c4950e6d8906da
Guilherme Watanabe <watanab430@gmail.com>
2021-07-16 20:37:52 -0300

    Fifth day.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import nookies from 'nookies';
+import jwt from 'jsonwebtoken';
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AluraKutCommons';
@@ -45,8 +47,8 @@ function ProfileRelationsBox(props) {
   );
 }
 
-export default function Home() {
-  const githubUser = 'GuilhermeWatanabe';
+export default function Home(props) {
+  const githubUser = props.githubUser;
   const [communities, setCommunity] = React.useState([]);
   const favorites = [
     'juunegreiros', 
@@ -195,4 +197,31 @@ export default function Home() {
       </MainGrid>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN;
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+        Authorization: token
+      }
+  })
+  .then((response) => response.json())
+
+  if(!isAuthenticated) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token);
+  return {
+    props: {
+      githubUser
+    }, 
+  }
 }
